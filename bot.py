8f69aba88593e5b900741cb703af7aecfd9de56f
Paul Salden <paul.salden@gmail.com>
2016-11-14 22:45:57 +0100

    split out async send loop

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import asyncio
+import collections
 import logging
 import socket
 import remotes
+import tasks
 from config.bot import settings as defaultsettings
 from errno import WSAEWOULDBLOCK  # EINPROGRESS
 from strings import encode, decode
@@ -12,41 +14,42 @@ RECEIVE_QUEUE_SIZE = 1024
 
 class Bot(object):
     def __init__(self, loop, settings):
-        self.settings = settings
-        self.remoteset = remotes.RemoteSet(self.send)
-
         self.loop = loop
+
+        self.settings = settings
+        self.tasks = tasks.TaskSet(self.loop)
+        self.remoteset = remotes.RemoteSet(self.send, self.tasks)
+
         self.reader = None
         self.writer = None
-
         self.read_buffer = b""
-        self.send_queue = []
+        self.send_queue = asyncio.Queue(loop=loop)
+        self.send_buffer = collections.deque()
 
         self.allow_send = True
         self.bytes_buffered = 0
 
         self.connect_success = False
 
-    # --- make and maintain connection ---
+    # --- connection handling ---
 
     async def mainloop(self):
         failed = 0
         # keep on trying to connect
         while True:
-            # try to connect and loop while connected
+            # try to connect and keep on receiving while connected
             if await self.connect():
+                send_future = asyncio.ensure_future(self.sendloop(), loop=self.loop)
                 while True:
-                    connected = await self.do_receive()
+                    connected = await self.receive_data()
 
                     if not connected:
                         logging.warning("Disconnected.")
+                        send_future.cancel()
                         self.writer.close()
-                        break
 
-                    await self.do_send()
-
-            # send _DISCONNECT pseudo-command for all loaded modules
-            self.remoteset.process("", "_DISCONNECT", "")
+                        # send _DISCONNECT pseudo-command for all loaded modules
+                        self.remoteset.process("", "_DISCONNECT", "")
 
             # exponentially delay reconnect if previous attempt(s) was/were unsuccessful
             if self.connect_success:
@@ -55,15 +58,16 @@ class Bot(object):
 
             else:
                 failed += 1
-                delay = 10**failed
+                delay = 10 ** failed
                 logging.info("Reconnecting in {} seconds.".format(delay))
                 await asyncio.sleep(delay)
 
     async def connect(self):
         # make sure queues, buffers, etc. are reset
+        self.reader = None
+        self.writer = None
         self.read_buffer = b""
-        self.buffer_out = b""
-        self.send_queue = []
+        self.send_queue = asyncio.Queue(loop=self.loop)
 
         self.allow_send = True
         self.bytes_buffered = 0
@@ -86,9 +90,17 @@ class Bot(object):
 
         return True
 
-    # --- receive and send data ---
+    async def sendloop(self):
+        data = b""
+        while True:
+            data += await self.send_queue.get()
 
-    async def do_receive(self):
+            if self.send_queue.empty():
+                self.writer.write(data)
+                await self.writer.drain()
+                data = b""
+
+    async def receive_data(self):
         data = await self.reader.read(8192)
 
         if not data:
@@ -126,36 +138,29 @@ class Bot(object):
 
         return True
 
-    async def do_send(self):
-        buffer_out = b""
-        while self.send_queue and self.allow_send:
-            line = self.send_queue.pop(0)
-            encoded_line = encode("{}\r\n".format(line))
-            encoded_line_len = len(encoded_line)
-
-            # wait for my commands to be processed after I have sent a critical amount of bytes
-            # note: I do not control the amount of bytes the server separately queues up to send me
-            if self.bytes_buffered + encoded_line_len > RECEIVE_QUEUE_SIZE:
-                self.allow_send = False
-                logging.info("-> SPLIDGEPLOIT")
-                buffer_out += encode("SPLIDGEPLOIT\r\n")
-
-            else:
-                logging.info("-> {}".format(line))
-                buffer_out += encoded_line
-                self.bytes_buffered += encoded_line_len
-
-        self.writer.write(buffer_out)
-        await self.writer.drain()
-
-    # --- further interaction ---
+    # --- interaction ---
 
     def basic_responses(self, command, args):
         # deal with response to anti-flood check
         if len(args) == 3 and (command, args[1]) == ("421", "SPLIDGEPLOIT"):
             self.bytes_buffered = 0
             self.allow_send = True
-            return
+
+            while len(self.send_buffer) > 0:
+                line = self.send_buffer.pop()
+                encoded_line = encode("{}\r\n".format(line))
+                encoded_line_len = len(encoded_line)
+
+                if self.bytes_buffered + encoded_line_len <= RECEIVE_QUEUE_SIZE:
+                    logging.info("-> {}".format(line))
+                    self.send_queue.put_nowait(encoded_line)
+                    self.bytes_buffered += encoded_line_len
+                else:
+                    self.send_buffer.append(line)
+                    self.allow_send = False
+                    logging.info("-> SPLIDGEPLOIT")
+                    self.send_queue.put_nowait(encode("SPLIDGEPLOIT\r\n"))
+                    break
 
         # respond to ping
         if command == "PING":
@@ -175,7 +180,25 @@ class Bot(object):
             return
 
     def send(self, line):
-        self.send_queue.append(line)
+        encoded_line = encode("{}\r\n".format(line))
+        encoded_line_len = len(encoded_line)
+
+        if self.allow_send:
+            # wait for my commands to be processed after I have sent a critical amount of bytes
+            # note: I do not control the amount of bytes the server separately queues up to send me
+            if self.bytes_buffered + encoded_line_len <= RECEIVE_QUEUE_SIZE:
+                logging.info("-> {}".format(line))
+                self.send_queue.put_nowait(encoded_line)
+                self.bytes_buffered += encoded_line_len
+                return
+
+            self.allow_send = False
+            logging.info("-> SPLIDGEPLOIT")
+            self.send_queue.put_nowait(encode("SPLIDGEPLOIT\r\n"))
+
+        self.send_buffer.appendleft(line)
+
+    # ----------
 
     def loadmodules(self):
         for m in self.settings["modules"]:
